b19dd7ede32774b121a460f58dde12aabded1410
Jessica <jesscara.993@gmail.com>
2023-01-20 16:33:31 +0100

    join 3

--- join_uni.sql
+++ join_uni.sql
@@ -8,12 +8,19 @@ WHERE `degrees`.`name` LIKE 'Corso di Laurea in Economia';
 -- -- 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
 SELECT * 
 FROM `degrees`
-JOIN `departments`
-ON `degrees`.`department_id` = `departments`.`id`
+  JOIN `departments`
+    ON `degrees`.`department_id` = `departments`.`id`
 WHERE `degrees`.`level` LIKE 'magistrale'
 AND `departments`.`name` LIKE 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `teachers`.`name`,`teachers`.`surname`,`courses`.*
+FROM `teachers`
+  JOIN `course_teacher`
+    ON `course_teacher`.`teacher_id` = `teachers`.`id`  
+  JOIN `courses`
+    ON `course_teacher`.`course_id` = `courses`.`id`
+WHERE `teachers`.`id` = 44;
 
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
